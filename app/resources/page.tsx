d0856839c2b6d9746084c5ac8210399b8ac0e8de
Aidan Tucker <tuckerai@umich.edu>
2023-12-05 21:34:23 -0500

    initial resources

--- app/resources/page.tsx
+++ app/resources/page.tsx
@@ -3,7 +3,14 @@ import Image from "next/image";
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24 bg-quaternary-grey">
-      Resources
+      <div>
+        <h2 className="font-semibold mb-4 text-2xl">The AI</h2>
+              <p className="max-w-5xl">
+                We leverage OpenAI's Dalle-3 with our own custom
+                UI to generate our picture or communication boards!</p><a href="https://openai.com/dall-e-3">
+                https://openai.com/dall-e-3
+              </a>
+      </div>
     </main>
   );
 }
